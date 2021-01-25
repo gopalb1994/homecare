5582615eaaac9032c7473e943976d1c0fcc5ffc8
Gopal Bbaskota <gopal.baskota94@gmail.com>
2021-01-25 09:26:01 -0500

    Contact fixed

--- src/Components/pages/contact/Form.js
+++ src/Components/pages/contact/Form.js
@@ -2,92 +2,118 @@ import React from "react";
 import "./Form.css";
 const Form = () => {
   return (
-    <div className="contact-form-container">
-      <div className="container">
-        <div className="innerwrap">
-          <section className="section1 clearfix">
-            <div className="textcenter mt-5">
-              <h1>Contact Us</h1>
+    <section className="Material-contact-section section-padding section-dark">
+      <div className="container ">
+        <div className="row ">
+          <div
+            className="col-md-12 wow animated fadeInLeft"
+            data-wow-delay=".2s"
+          >
+            <h1 className="section-title text-center contact-section-top">
+              Love to Hear From You
+            </h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6 mt-3 d-flex justify-content-center flex-column contact-widget-section2 wow animated fadeInLeft">
+            <div className="find-widget">
+              Address: <a href="#">4400 Linglestown rd, Harrisburg, PA 17112</a>
             </div>
-          </section>
-          <section className="section2 clearfix">
-            <div className="col2 column1 first ">
-              <div
-                className="sec2map d-flex justify-content-center align-items-center address-info"
-                style={{ overflow: "hidden", height: "550px", width: "100%" }}
-                d-flex
-                justify-content-center
-                align-items-center
-              >
-                <div className="sec2innercont">
-                  <div className="sec2addr">
-                    <p>
-                      45 BC, a Latin professor at Hampden-Sydney College in
-                      Virginia
-                    </p>
-                    <p>
-                      <span className="collig">Phone :</span> +91 976885083
-                    </p>
-                    <p>
-                      <span className="collig">Email :</span>{" "}
-                      vivek.mengu016@gmail.com
-                    </p>
-                    <p>
-                      <span className="collig">Fax :</span> +91 9768850839
-                    </p>
-                  </div>
-                </div>
+            <div className="find-widget">
+              Phone: <a href="#">717-204-3200</a>
+            </div>
+            <div className="find-widget">
+              Website:{" "}
+              <a href="https://dreamhomecare.net">www.dreamhomecare.net</a>
+            </div>
+            <div className="find-widget mb-5">
+              Program: <a href="#">Mon to fri: 09:30 AM - 5:00PM</a>
+            </div>
+          </div>
+          <div
+            className="col-md-6 wow animated fadeInRight"
+            data-wow-delay=".2s"
+          >
+            <form
+              className="shake"
+              role="form"
+              method="post"
+              id="contactForm"
+              name="contact-form"
+              data-toggle="validator"
+            >
+              <div className="form-group label-floating">
+                <label className="control-label" htmlFor="name">
+                  Name
+                </label>
+                <input
+                  className="form-control"
+                  id="name"
+                  type="text"
+                  name="name"
+                  required
+                  data-error="Please enter your name"
+                />
+                <div className="help-block with-errors" />
               </div>
-            </div>
-            <div className="col2 column2 last main-form">
-              <div className="sec2contactform">
-                <h3 className="sec2frmtitle">
-                  Want to Know More?? Drop Us a Mail
-                </h3>
-                <form action>
-                  <div className="clearfix">
-                    <input
-                      className="col2 first"
-                      type="text"
-                      placeholder="FirstName"
-                    />
-                    <input
-                      className="col2 last"
-                      type="text"
-                      placeholder="LastName"
-                    />
-                  </div>
-                  <div className="clearfix">
-                    <input
-                      className="col2 first"
-                      type="Email"
-                      placeholder="Email"
-                    />
-                    <input
-                      className="col2 last"
-                      type="text"
-                      placeholder="Contact Number"
-                    />
-                  </div>
-                  <div className="clearfix">
-                    <textarea
-                      name="textarea"
-                      id
-                      cols={30}
-                      rows={7}
-                      defaultValue={"Your message here..."}
-                    />
-                  </div>
-                  <div className="clearfix">
-                    <input type="submit" defaultValue="Send" />
-                  </div>
-                </form>
+              <div className="form-group label-floating">
+                <label className="control-label" htmlFor="email">
+                  Email
+                </label>
+                <input
+                  className="form-control"
+                  id="email"
+                  type="email"
+                  name="email"
+                  required
+                  data-error="Please enter your Email"
+                />
+                <div className="help-block with-errors" />
               </div>
-            </div>
-          </section>
+              <div className="form-group label-floating">
+                <label className="control-label">Subject</label>
+                <input
+                  className="form-control"
+                  id="msg_subject"
+                  type="text"
+                  name="subject"
+                  required
+                  data-error="Please enter your message subject"
+                />
+                <div className="help-block with-errors" />
+              </div>
+              <div className="form-group label-floating">
+                <label htmlFor="message" className="control-label">
+                  Message
+                </label>
+                <textarea
+                  className="form-control"
+                  rows={3}
+                  id="message"
+                  name="message"
+                  required
+                  data-error="Write your message"
+                  defaultValue={""}
+                />
+                <div className="help-block with-errors" />
+              </div>
+              <div className="form-submit mt-5">
+                <button
+                  className="btn btn-primary"
+                  type="submit"
+                  id="form-submit"
+                >
+                  <i className="material-icons mdi mdi-message-outline" /> Send
+                  Message
+                </button>
+                <div id="msgSubmit" className="h3 text-center hidden" />
+                <div className="clearfix" />
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
